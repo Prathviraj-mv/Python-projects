dc01bdba51352be6a75c647dd572678129c9dfb4
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-05-13 11:07:15 +0530

    Update nasa.py

--- NASA api astronomy picture of the day/nasa.py
+++ NASA api astronomy picture of the day/nasa.py
@@ -1,1 +1,19 @@
+import requests
+
+# api key can be found in nasa website
+# lastest astromony picture of the day will be displayed 
+
+
+api_end = "https://api.nasa.gov/planetary/apod"
+params = {
+    "api_key": "ur api key"
+}
+
+response = requests.get(api_end, params=params)
+data = response.json()
+image_url = data["hdurl"]
+print(image_url)
+
+
+
 
