7bae170f85c61f7b9af3f059d7f86a948ce75197
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-05-13 11:03:58 +0530

    Update download.py

--- Song from YouTube video downloader/download.py
+++ Song from YouTube video downloader/download.py
@@ -1,1 +1,22 @@
+import os
+import pytube
+# download oly audio(mp3) from Youtube.com
+from pytube import YouTube
+
+# set ur folder
+path = "C:/yt"
+
+
+def video_download():
+    link = input("LINK:")
+    video_link = YouTube(str(link))
+
+    video = video_link.streams.last()
+    video.download(path)
+
+
+video_download()
+
+# folder opens automatically
+os.startfile("C:/yt")
 
