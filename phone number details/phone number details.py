28daf9e685ce8fe2b08f7031613991e9ffdda547
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-05-13 11:02:19 +0530

    Update phone number details.py

--- phone number details/phone number details.py
+++ phone number details/phone number details.py
@@ -1,1 +1,21 @@
+import phonenumbers
+
+from phonenumbers import timezone, geocoder, carrier
+
+
+def details():
+    number = input("ENTER THE NUMBER \n"
+                   " + (countrycode)(number)")
+    phone = phonenumbers.parse(number, None)
+    time = timezone.time_zones_for_number(phone)
+    provider = carrier.name_for_number(phone, "en")
+    geo = geocoder.description_for_number(phone, "en")
+
+    print(phone)
+    print(time)
+    print(provider)
+    print(geo)
+
+
+details()
 
