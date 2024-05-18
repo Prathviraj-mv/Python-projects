48b53e705759b999b0241ea7fa911f6abd360a39
Prathvirajmv <168354749+chappie404error@users.noreply.github.com>
2024-05-18 22:04:21 +0530

    Update nato_phonetic_alphabet.py

--- nato phonetics/nato_phonetic_alphabet.py
+++ nato phonetics/nato_phonetic_alphabet.py
@@ -1,5 +1,9 @@
 words = {
-    'A': 'Alfa', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo', 'F': 'Foxtrot', 'G': 'Golf', 'H': 'Hotel', 'I': 'India', 'J': 'Juliet', 'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November', 'O': 'Oscar', 'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 'S': 'Sierra', 'T': 'Tango', 'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray', 'Y': 'Yankee', 'Z': 'Zulu'
+    'A': 'Alfa', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo', 'F': 'Foxtrot',
+    'G': 'Golf', 'H': 'Hotel', 'I': 'India', 'J': 'Juliet', 'K': 'Kilo', 'L': 'Lima', 
+    'M': 'Mike', 'N': 'November', 'O': 'Oscar', 'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo',
+    'S': 'Sierra', 'T': 'Tango', 'U': 'Uniform', 'V': 'Victor', 'W': 'Whiskey',
+    'X': 'X-ray', 'Y': 'Yankee', 'Z': 'Zulu'
 }
 nato_commands = ""
 for letter in input(":").upper():
